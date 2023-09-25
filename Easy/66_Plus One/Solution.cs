b604b550f5f5207a60606a0128371021ef2be40c
AlexeySH8 <shevchenkowe@gmail.com>
2023-09-25 23:11:25 +0600

    Create Solution.cs

---
+++ Easy/66_Plus One/Solution.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        if (digits.Length == 0) return new int[] { };
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        var result = new int[digits.Length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
