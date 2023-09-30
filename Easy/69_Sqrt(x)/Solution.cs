07e7b9a663cf79ee2c6656f5906332563f140b5e
AlexeySH8 <shevchenkowe@gmail.com>
2023-09-30 15:14:19 +0600

    Create Solution.cs

---
+++ Easy/69_Sqrt(x)/Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int MySqrt(int num)
+    {
+        if (num == 0) return 0;
+        if (num == 1) return 1;
+        double x = num / 2;
+        int stop = 0;
+        while (x * x != num)
+        {
+            x = (x + (num / x)) / 2;
+            if (stop == Math.Floor(x))
+                return (int)x;
+            stop = (int)x;
+        }
+        return (int)x;
+    }
+}
