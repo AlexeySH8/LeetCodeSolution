02847734795f66c1dcc84bd7dc3629ab7b8be9cb
AlexeySH8 <shevchenkowe@gmail.com>
2023-10-15 22:35:54 +0600

    88_Merge Sorted Array

---
+++ Easy/88_Merge Sorted Array/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public void Merge(int[] basik, int basikIndex, int[] sub, int subIndex)
+    {
+        for (int i = basik.Length - 1; i >= 0; i--)
+        {
+            if ((subIndex <= 0) ||
+                (basikIndex > 0 && basik[basikIndex - 1] > sub[subIndex - 1]))
+            {
+                basik[i] = basik[basikIndex - 1];
+                basikIndex--;
+            }
+            else
+            {
+                basik[i] = sub[subIndex - 1];
+                subIndex--;
+            }
+        }
+    }
+}
