bfbdde8336c15f0c9ec68e87b71a9e357d68fc0c
AlexeySH8 <shevchenkowe@gmail.com>
2023-10-18 11:25:11 +0600

    3_Longest Substring Without Repeating Characters

---
+++ Medium/3_Longest Substring Without Repeating Characters/Solution.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var list = new List<char>();
+        int index = 0, max = 0;
+
+        while (index < s.Length)
+        {
+            if (index < s.Length && !list.Contains(s[index]))
+            {
+                list.Add(s[index++]);
+                if (list.Count > max)
+                    max = list.Count;
+            }
+            else
+                list.RemoveAt(0);
+        }
+        return max;
+    }
+}
