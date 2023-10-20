9cd3e38d7060174fa7c65736bacd7b0a4e0eaae2
AlexeySH8 <shevchenkowe@gmail.com>
2023-10-20 23:54:26 +0600

    Container With Most Water

---
+++ Medium/11_Container With Most Water/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int left = 0, right = height.Length - 1;
+        int result = 0, temp = 0;
+
+        while (left < right)
+        {
+            temp = (right - left) * Math.Min(height[left], height[right]);
+
+            if (height[right] > height[left])
+                left++;
+            else
+                right--;
+            result = Math.Max(result, temp);
+        }
+        return result;
+    }
+}
