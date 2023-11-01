799e2c7db9f82016d2853369f6d7c90ce54bc428
AlexeySH8 <shevchenkowe@gmail.com>
2023-11-01 22:08:53 +0600

    17_Letter Combinations of a Phone Number

---
+++ Medium/17_Letter Combinations of a Phone Number/Solution.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+public class Solution
+{
+    private new List<string> result = new List<string>();
+    private static Dictionary<char, string> LettersInNum = new Dictionary<char, string>
+    {
+            { '2', "abc" },
+            { '3', "def" },
+            { '4', "ghi" },
+            { '5', "jkl" },
+            { '6', "mno" },
+            { '7', "pqrs" },
+            { '8', "tuv" },
+            { '9', "wxyz" }
+    };
+
+    public IList<string> LetterCombinations(string digits)
+    {
+        if (digits == null || digits.Length == 0) return new List<string>();
+        MakePermutation(digits, 0, new StringBuilder());
+        return result;
+    }
+
+    public void MakePermutation(string digits, int indexOfKey,
+        StringBuilder builder)
+    {
+        if (indexOfKey > digits.Length - 1)
+        {
+            result.Add(builder.ToString());
+            return;
+        }
+
+        foreach (char currentLetter in LettersInNum[digits[indexOfKey]])
+        {
+            builder.Append(currentLetter);
+            MakePermutation(digits, indexOfKey + 1, builder);
+            builder.Remove(builder.Length - 1, 1);
+        }
+    }
+}
