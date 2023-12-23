81e3a66b2d62cfcbace126362b3e3f1bf79426bb
AlexeySH8 <shevchenkowe@gmail.com>
2023-12-23 16:30:34 +0600

    94_Binary Tree Inorder Traversal

---
+++ Easy/94_Binary Tree Inorder Traversal/Solution.cs
@@ -0,0 +1,22 @@
+﻿using Class_Work;
+
+public class Solution
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        var list = new List<int>();
+        if (root != null)
+        {
+            if (root.left != null)
+            {
+                list.AddRange(InorderTraversal(root.left));
+            }
+            list.Add(root.val);
+            if (root.right != null)
+            {
+                list.AddRange(InorderTraversal(root.right));
+            }
+        }
+        return list;
+    }
+}
