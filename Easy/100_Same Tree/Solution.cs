8e9566caaf2f9dbfd64093a5a0550c8d2ac5d8f2
AlexeySH8 <shevchenkowe@gmail.com>
2023-12-26 11:03:41 +0600

    100_Same Tree

---
+++ Easy/100_Same Tree/Solution.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public bool IsSameTree(TreeNode root1, TreeNode root2)
+    {
+        if (root1 == null && root2 == null) return true;
+
+        if (root1 != null && root2 != null)
+        {
+            return root1.val == root2.val &&
+                IsSameTree(root1.left, root2.left) &&
+                IsSameTree(root1.right, root2.right);
+        }
+        return false;
+    }
+}
