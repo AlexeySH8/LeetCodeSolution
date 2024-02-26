1fe6b0874fb83df0beed5e546e0ba3f90bfc70e0
AlexeySH8 <shevchenkowe@gmail.com>
2024-02-26 16:41:09 +0600

    242. Valid Anagram

---
+++ Easy/242. Valid Anagram/Solution.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+        var dicS = MakeDictionary(s);
+        var dicT = MakeDictionary(t);
+        return AreDictionariesEqual(dicS, dicT);
+    }
+
+    static bool AreDictionariesEqual(Dictionary<char, int> dict1, Dictionary<char, int> dict2)
+    {
+        foreach (var key in dict1.Keys)
+            if (!dict2.ContainsKey(key) || !dict2[key].Equals(dict1[key]))
+                return false;
+        return true;
+    }
+
+    private static Dictionary<char, int> MakeDictionary(string str)
+    {
+        var result = new Dictionary<char, int>();
+        foreach (var chr in str)
+        {
+            if (result.ContainsKey(chr))
+                result[chr]++;
+            else result.Add(chr, 1);
+        }
+        return result;
+    }
+}
