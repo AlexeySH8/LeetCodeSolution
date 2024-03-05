03cc261bea3287453d20b15e7e86034cf74fe34e
AlexeySH8 <shevchenkowe@gmail.com>
2024-03-05 20:14:10 +0500

    49. Group Anagrams

---
+++ Medium/49.Group Anagrams/Solution.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    private Dictionary<string, List<string>> dicAnagrams = new Dictionary<string, List<string>>();
+
+    public IList<IList<string>> GroupAnagrams(string[] strs)
+    {
+        foreach (string anagram in strs)
+        {
+            var key = new string (anagram
+                .OrderBy(x => x)
+                .ToArray());
+
+            if (!dicAnagrams.ContainsKey(key))
+                dicAnagrams.Add(key, new List<string>());
+
+            dicAnagrams[key].Add(anagram);
+        }
+
+        return dicAnagrams
+            .Select(x => (IList<string>)x.Value)
+            .ToList();
+    }
+}
