0e9ce8bd119a13f4311df18a101ce66b7ca58ae2
AlexeySH8 <shevchenkowe@gmail.com>
2024-03-05 18:29:25 +0500

    1.Two Sum

---
+++ Easy/1.Two Sum/Solution.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        var values = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            var deltaNum = target - nums[i];
+            if (values.ContainsKey(deltaNum))
+                return new int[] { values[deltaNum], i };
+            values.TryAdd(nums[i], i);
+        }
+        return default;
+    }
+}
