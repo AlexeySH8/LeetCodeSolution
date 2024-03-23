f2a865bdc935d15fea528ef01238725e0553e3e5
AlexeySH8 <shevchenkowe@gmail.com>
2024-03-23 18:49:37 +0500

    347.Top K Frequent Elements

---
+++ Medium/347.Top K Frequent Elements/Solution.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public int[] TopKFrequent(int[] nums, int k)
+    {
+        var result = new int[k];
+        var queue = new PriorityQueue<int, int>();
+        var dict = nums
+            .GroupBy(x => x)
+            .ToDictionary(group => group.Key, group => group.Count());
+
+        foreach (var item in dict)
+            queue.Enqueue(item.Key, -item.Value);
+
+        for (int i = 0; i < k; i++)
+            result[i] = queue.Dequeue();
+
+        return result;
+    }
+}
