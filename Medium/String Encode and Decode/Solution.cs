86ba4a5f1e84924ed57de909e78b651bb16abbeb
AlexeySH8 <shevchenkowe@gmail.com>
2024-03-26 15:37:16 +0500

    String Encode and Decode

---
+++ Medium/String Encode and Decode/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string Encode(IList<string> strs)
+    {
+        var builder = new StringBuilder();
+        foreach (var str in strs)
+            builder.Append(str.Length).Append('/').Append(str);
+        return builder.ToString();
+    }
+
+    public List<string> Decode(string s)
+    {
+        var result = new List<string>();
+        int index = 0;
+        while (index < s.Length)
+        {
+            int slashIndex = s.IndexOf("/", index);
+            int subStringLength = int.Parse(s.Substring(index, slashIndex - index));
+            result.Add(s.Substring(slashIndex + 1, subStringLength));
+            index = slashIndex + subStringLength + 1;
+        }
+        return result;
+    }
+}
