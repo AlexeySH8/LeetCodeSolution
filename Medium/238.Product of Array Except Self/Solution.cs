8941ef2f2acb9ed88712a0f72d40b00198313049
AlexeySH8 <shevchenkowe@gmail.com>
2024-03-27 17:55:08 +0500

    238.Product of Array Except Self

---
+++ Medium/238.Product of Array Except Self/Solution.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int[] ProductExceptSelf(int[] nums)
+    {
+        int length = nums.Length;
+        var result = new int[length];
+        int sufix = 1;
+        result[0] = 1;
+
+        for (int i = 1; i < length; i++)
+            result[i] = result[i - 1] * nums[i - 1];
+
+        for (int i = length - 1; i >= 0; i--)
+        {
+            result[i] *= sufix;
+            sufix *= nums[i];
+        }
+        return result;
+    }
+}
