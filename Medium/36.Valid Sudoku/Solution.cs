c89d1135ad04d9a2ddf8eaf21737a5df100ddcc7
AlexeySH8 <shevchenkowe@gmail.com>
2024-03-28 16:24:09 +0500

    36.Valid Sudoku

---
+++ Medium/36.Valid Sudoku/Solution.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public bool IsValidSudoku(char[][] board)
+    {
+        var existingValues = new HashSet<string>();
+        for (int i = 0; i < board.Length; i++)
+        {
+            for (int j = 0; j < board.Length; j++)
+            {
+                if (board[i][j] == '.') continue;
+                else if (!existingValues.Add(board[i][j] + " row " + j) ||
+                    !existingValues.Add(board[i][j] + " coulmn " + i) ||
+                    !existingValues.Add(board[i][j] + " " + i / 3 +
+                    " - " + j / 3 + " box"))
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
