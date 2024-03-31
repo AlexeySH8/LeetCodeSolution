8390791f6e704014c296eac51fecfb6c2e393a64
AlexeySH8 <shevchenkowe@gmail.com>
2024-03-31 19:20:19 +0500

    20.Valid Parentheses

---
+++ Easy/20.Valid Parentheses/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    private Dictionary<char, char> pair = new Dictionary<char, char>()
+    {
+        {'(' , ')' },
+        {'[' , ']'},
+        {'{' , '}'}
+    };
+
+    public bool IsValid(string s)
+    {
+        var stack = new Stack<char>();
+        foreach (var item in s)
+        {
+            if (pair.ContainsKey(item))
+                stack.Push(item);
+            else
+            {
+                if (stack.Count < 1 ||
+                    !(pair[stack.Pop()] == item))
+                    return false;
+            }
+        }
+        return stack.Count == 0;
+    }
+}
