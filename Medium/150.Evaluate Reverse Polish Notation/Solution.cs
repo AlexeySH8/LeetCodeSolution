9cf65d02ad50517b8e659b09194812485af87e8e
AlexeySH8 <shevchenkowe@gmail.com>
2024-04-05 13:30:54 +0500

    150.Evaluate Reverse Polish Notation

---
+++ Medium/150.Evaluate Reverse Polish Notation/Solution.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    private Stack<int> values = new Stack<int>();
+
+    public int EvalRPN(string[] tokens)
+    {
+        foreach (var token in tokens)
+        {
+            if (int.TryParse(token, out int vlaue))
+                values.Push(vlaue);
+            else
+                values.Push(DoCalculation(values.Pop(),
+                    values.Pop(), token));
+        }
+        return values.First();
+    }
+
+    private int DoCalculation(int b, int a, string operation) =>
+        operation switch
+        {
+            "+" => a + b,
+            "-" => a - b,
+            "*" => a * b,
+            "/" => a / b,
+            _ => throw new Exception(operation)
+        };
+
+}
