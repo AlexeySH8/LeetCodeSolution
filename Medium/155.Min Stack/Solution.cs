8e29c09fe27df0ba9d8a9750802e287c35d58e04
AlexeySH8 <shevchenkowe@gmail.com>
2024-04-05 13:08:36 +0500

    155.Min Stack

---
+++ Medium/155.Min Stack/Solution.cs
@@ -0,0 +1,36 @@
+﻿public class MinStack
+{
+    private List<int> list;
+    private Stack<int> minValue;
+    public MinStack()
+    {
+        list = new List<int>();
+        minValue = new Stack<int>();
+    }
+
+    public void Push(int val)
+    {
+        var temp = val;
+        if (minValue.Count == 0 ||
+            temp <= minValue.First())
+            minValue.Push(temp);
+        list.Add(val);
+    }
+
+    public void Pop()
+    {
+        if (list.Last() == minValue.First())
+            minValue.Pop();
+        list.RemoveAt(list.Count - 1);
+    }
+
+    public int Top()
+    {
+        return list.Last();
+    }
+
+    public int GetMin()
+    {
+        return minValue.First();
+    }
+}
