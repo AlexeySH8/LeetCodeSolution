18eb185bcbfc1c296f21b516446a6d00ccbd2742
AlexeySH8 <shevchenkowe@gmail.com>
2024-04-09 13:03:21 +0500

    739.Daily Temperatures

---
+++ Medium/739.Daily Temperatures/Solution.cs
@@ -0,0 +1,18 @@
+﻿public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        var result = new int[temperatures.Length];
+        var stack = new Stack<int>();
+
+        for (int day = 0; day < temperatures.Length; day++)
+        {
+            while (stack.Count > 0 &&
+                temperatures[day] > temperatures[stack.Peek()])
+                result[stack.Peek()] = day - stack.Pop();
+
+            stack.Push(day);
+        }
+        return result;
+    }
+}
