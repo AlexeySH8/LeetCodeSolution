6b891a609622600fb4ebcf73e122c34496253246
AlexeySH8 <shevchenkowe@gmail.com>
2024-04-13 13:26:26 +0500

    22. Generate Parentheses

---
+++ Medium/22. Generate Parentheses/Solution.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        var list = new List<string>();
+        GenerateParenthesisList(list, "", 0, 0, n);
+        return list;
+    }
+
+    private void GenerateParenthesisList(List<string> result,
+        string current, int open, int close, int max)
+    {
+        if (current.Length == max * 2)
+        {
+            result.Add(current);
+            return;
+        }
+
+        if (open < max)
+            GenerateParenthesisList(result, current + "(", open + 1, close, max);
+
+
+        if (close < open)
+            GenerateParenthesisList(result, current + ")", open, close + 1, max);
+    }
+}
