90523b073938e7f8b4816767e731481409570c75
AlexeySH8 <shevchenkowe@gmail.com>
2024-04-13 15:28:26 +0500

    853.Car Fleet

---
+++ Medium/853.Car Fleet/Solution.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int CarFleet(int target, int[] position, int[] speed)
+    {
+        Array.Sort(position, speed);
+        var stack = new Stack<float>();
+        for (int car = 0; car < position.Length; car++)
+        {
+            var time = (float)(target - position[car]) / speed[car];
+            while (stack.Count > 0 && stack.Peek() <= time)
+                stack.Pop();
+            stack.Push(time);
+        }
+        return stack.Count;
+    }
+}
