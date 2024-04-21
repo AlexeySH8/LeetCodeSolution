997133a1b87f881362604d2c0ca1bdc2d46e2f5d
AlexeySH8 <shevchenkowe@gmail.com>
2024-04-21 20:58:58 +0500

    125. Valid Palindrome

---
+++ Easy/125. Valid Palindrome/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.RegularExpressions;
+
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        if (s.Length == 1) return true;
+        s = RemoveNonAlphabeticCharacters(s).ToLower();
+        int left = 0;
+        int right = s.Length - 1;
+        while (left < right)
+        {
+            if (s[left] != s[right])
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private string RemoveNonAlphabeticCharacters(string s)
+    {
+        return Regex.Replace(s, @"[^\p{L}\p{N}]", "");
+    }
+}
