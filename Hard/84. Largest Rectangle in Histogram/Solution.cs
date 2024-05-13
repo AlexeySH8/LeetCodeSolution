2794b3bae7793b9f4f466670a57e07f7bec230c4
AlexeySH8 <shevchenkowe@gmail.com>
2024-05-13 15:15:01 +0500

    Create Solution.cs

---
+++ Hard/84. Largest Rectangle in Histogram/Solution.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public int LargestRectangleArea(int[] heights)
+    {
+        int max = 0;
+        var stack = new Stack<int>();
+        for (int i = 0; i <= heights.Length; i++)
+        {
+            int h = (i == heights.Length) ? 0 : heights[i];
+
+            if (stack.Count == 0 || h >= heights[stack.Peek()])
+                stack.Push(i);
+            else
+            {
+                while (stack.Count != 0 && h < heights[stack.Peek()])
+                {
+                    var length = heights[stack.Pop()];
+                    int width = (stack.Count == 0) ? i : i - stack.Peek() - 1;
+                    var temp = width * length;
+                    max = Math.Max(max, temp);
+                }
+                stack.Push(i);
+            }
+        }
+        return max;
+    }
+}
