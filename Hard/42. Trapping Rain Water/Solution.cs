b089f0ef447c3143921f416ce49cbbcc736a6bd3
AlexeySH8 <shevchenkowe@gmail.com>
2024-06-25 18:02:35 +0500

    Create Solution.cs

---
+++ Hard/42. Trapping Rain Water/Solution.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public int Trap(int[] height)
+    {
+        int water = 0;
+        int left = 0;
+        int right = height.Length - 1;
+        int maxLeft = height[left];
+        int maxRight = height[right];
+
+        while (left < right - 1)
+        {
+            if (height[left] < height[right])
+            {
+                left++;
+                if (height[left] < maxLeft)
+                {
+                    water += maxLeft - height[left];
+                }
+                else maxLeft = height[left];
+            }
+            else
+            {
+                right--;
+                if (height[right] < maxRight)
+                {
+                    water += maxRight - height[right];
+                }
+                else maxRight = height[right];
+            }
+        }
+        return water;
+    }
+}
